05aa22d0361b59834f65cbc7016a9acfad8fb8c5
Qi Deng <s1340541@live.hkmu.edu.hk>
2025-11-19 20:18:21 +0800

    Refactor userSchema to postSchema with validations

--- models/posts.js
+++ models/posts.js
@@ -1,11 +1,38 @@
 var mongoose = require('mongoose');
 
-var userSchema = new mongoose.Schema({
-    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}, // Reference to the User model
-    caption: {type: String, required: false}, // Caption for the post
-    images: [{type: String, required: false}], // Array of image URLs
-    tags: [{type: String, required: false}], // Array of tags
-    likeCount: {type: Number, default: 0}, // Number of likes
+var postSchema = mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    content: {
+        type: String,
+        required: true,
+        maxlength: 2000
+    },
+    images: {
+        type: [String],
+        required: true,
+        validate: {
+            validator: function(v) {
+                return v && v.length > 0 && v.length <= 10;
+            },
+            message: 'Posts must contain 1-10 images'
+        }
+    },
+    tags: {
+        type: [String],
+        default: []
+    },
+    likeCount: {
+        type: Number,
+        default: 0,
+        min: 0
+    }
 });
 
-module.exports = mongoose.model('Post', userSchema);
+postSchema.index({ userId: 1 });
+postSchema.index({ tags: 1 });
+
+module.exports = mongoose.model('Post', postSchema);
