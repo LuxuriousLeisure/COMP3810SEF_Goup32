d44bc845658bd0bac9eb34b7b45dcf1d7ce7dcf2
WUYou <159100793+LuxuriousLeisure@users.noreply.github.com>
2025-11-19 20:08:54 +0800

    Add files via upload

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var userSchema = new mongoose.Schema({
     username:{type: String, required: true}, // Username of the user
     password: {type: String, required: true}, // Hashed? password
+    profileImage: {type: String, required: false}, // URL of the profile image
 });
 
 module.exports = mongoose.model('User', userSchema);
