111210729a56be86ae2ec8a701bde4be4e903ae2
Qi Deng <s1340541@live.hkmu.edu.hk>
2025-11-19 20:18:46 +0800

    Update comments.js

--- models/comments.js
+++ models/comments.js
@@ -1,9 +1,23 @@
 var mongoose = require('mongoose');
 
-var userSchema = new mongoose.Schema({
-    postId: {type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true}, // Reference to the Post model
-    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}, // Reference to the User model
-    content: {type: String, required: true}, // Content of the comment
+var commentSchema = mongoose.Schema({
+    postId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post',
+        required: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    content: {
+        type: String,
+        required: true,
+        maxlength: 500
+    }
 });
 
-module.exports = mongoose.model('Comment', userSchema);
+commentSchema.index({ postId: 1 });
+
+module.exports = mongoose.model('Comment', commentSchema);
