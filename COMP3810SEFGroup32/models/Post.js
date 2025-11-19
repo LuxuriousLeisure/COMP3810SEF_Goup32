c54d185194182a85a502afe29369997f2bc96f92
Andy30303000 <146706973+Andy30303000@users.noreply.github.com>
2025-11-19 13:13:15 +0800

    Delete COMP3810SEFGroup32 directory

--- COMP3810SEFGroup32/models/Post.js
+++
@@ -1,8 +0,0 @@
-// models/Post.js
-const mongoose = require('mongoose');
-module.exports = mongoose.model('Post', new mongoose.Schema({
-  image: { type: String, required: true },
-  caption: String,
-  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
-}, { timestamps: true }));
--- COMP3810SEFGroup32/models/User.js
+++
@@ -1,6 +0,0 @@
-// models/User.js
-const mongoose = require('mongoose');
-module.exports = mongoose.model('User', new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
-}));
