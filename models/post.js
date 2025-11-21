6ddff755344662231c434cfe2507dead581bedb8
Andy30303000 <146706973+Andy30303000@users.noreply.github.com>
2025-11-21 11:42:02 +0800

    Add files via upload

---
+++ models/post.js
@@ -0,0 +1,38 @@
+var mongoose = require('mongoose');
+
+var postSchema = mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    content: {
+        type: String,
+        required: true,
+        maxlength: 2000
+    },
+    images: {
+        type: [String],
+        required: true,
+        validate: {
+            validator: function(v) {
+                return v && v.length > 0 && v.length <= 10;
+            },
+            message: 'Posts must contain 1-10 images'
+        }
+    },
+    tags: {
+        type: [String],
+        default: []
+    },
+    likeCount: {
+        type: Number,
+        default: 0,
+        min: 0
+    }
+});
+
+postSchema.index({ userId: 1 });
+postSchema.index({ tags: 1 });
+
+module.exports = postSchema;
